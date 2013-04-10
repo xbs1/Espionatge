12f8b18441a0012c6f47e6eb26a2354827b16705
Adán Guerrero <agg9@alumnes.udl.cat>
2013-04-10 12:48:39 +0200

    Added model pages

--- Espionatge/urls.py
+++ Espionatge/urls.py
@@ -6,7 +6,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', views.mainpage, name='home'),
+    url(r'^$', views.mainpage, name='home'),
+    url(r'^cases/', views.cases, name='cases'),
+    url(r'^clients/', views.clients, name='clients'),
+    url(r'^detectives/', views.detectives, name='detectives'),
+    url)r'^suspects/', views.suspects, name='suspects'),
     # url(r'^Espionatge/', include('Espionatge.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
