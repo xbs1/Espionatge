c626c244ecda3d3cfb470b767c1bf5f0a1eebdaf
root <root@xavi-VirtualBox.(none)>
2013-04-12 22:26:40 +0200

    "json parser"

--- Espionatge/urls.py
+++ Espionatge/urls.py
@@ -19,6 +19,8 @@ urlpatterns = patterns('',
     
 
     
+
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- appEspionatge/jsonUtils.py
+++ appEspionatge/jsonUtils.py
@@ -1,3 +1,15 @@
+from django.utils import simplejson
+
 def json_list_all_links(model, path, request):
-	return "json string"
+
+	paths = []
+	
+	for instance in model.objects.all():
+		paths.append(instance.get_path(request))
+
+	data = simplejson.dumps({
+	'links': paths
+	})
+	
+	return data
 	
